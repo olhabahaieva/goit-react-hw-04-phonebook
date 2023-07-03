22a957c2cc483e01ed84b2e60a357aaa43821041
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-07-03 17:19:10 +0300

    App uses hooks instead of classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Phonebook from './Phonebook';
 import Contacts from './Contacts';
 import Filter from './Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = ({ contacts, filter }) => {
+  const [contactsState, setContacts] = useState([]);
+  const [filterState, setFilter] = useState('');
 
   // Filter set state function
-  handleFilterClick = (e) => {
-    this.setState({
-      filter: e.target.value,
-    });
+  const handleFilterClick = e => {
+    setFilter(e.target.value);
   };
 
   // Enter contact name and number function
-  handlePhonebookClick = (inputName, inputNumber) => {
-    const { contacts } = this.state;
-    const existingContact = contacts.find(
-      (contact) => contact.name.toLowerCase() === inputName.toLowerCase()
+  const handlePhonebookClick = (inputName, inputNumber, contactsState) => {
+    const existingContact = contactsState.find(
+      contact => contact.name.toLowerCase() === inputName.toLowerCase()
     );
 
     if (existingContact) {
@@ -31,59 +26,45 @@ export class App extends Component {
         name: inputName,
         number: inputNumber,
       };
-
-      this.setState((prevState) => ({
-        contacts: [...prevState.contacts, newContact],
-      }));
+      setContacts([...contacts, newContact]);
     }
   };
 
-  handleContactDelete = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== id),
-    }));
+  const handleContactDelete = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  componentDidMount() {
+  useEffect(() => {
     const savedContacts = localStorage.getItem('PhonebookContacts');
 
     if (savedContacts) {
-      this.setState({
-        contacts: JSON.parse(savedContacts),
-      });
+      setContacts(JSON.parse(savedContacts));
     }
-  }
+  }, [contacts, contactsState]);
 
-  render() {
-    const { contacts, filter } = this.state;
+  // Filter the contacts based on the filter state
+  const filteredContacts = contactsState.filter(contact =>
+    contact.name.toLowerCase().includes(filterState.toLowerCase())
+  );
 
-    // Filter the contacts based on the filter state
-    const filteredContacts = contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Phonebook
-          createContact={this.handlePhonebookClick}
-          contacts={contacts}
-        />
-        <Filter onChange={this.handleFilterClick} />
-        <Contacts
-          contacts={filteredContacts}
-          onDeleteContact={this.handleContactDelete}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Phonebook createContact={handlePhonebookClick} contacts={contacts} />
+      <Filter onChange={handleFilterClick} />
+      <Contacts
+        contacts={filteredContacts}
+        onDeleteContact={handleContactDelete}
+      />
+    </div>
+  );
+};
